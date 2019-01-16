094e0839deb0ec175c13a4cf04fe9838e971bfb9
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-01-16 22:54:58 +0100

    Agent selection pipeline

---
+++ src/agent-selection.pipeline.ts
@@ -0,0 +1,20 @@
+import {Agent} from './agent';
+import {Pipeline} from './pipeline';
+
+/**
+ * Concrete pipeline
+ * @export
+ * @class AgentSelectionPipeline
+ * @extends {Pipeline<Agent>}
+ */
+export class AgentSelectionPipeline extends Pipeline<Agent> {
+  /**
+   * Method which executes the filter on a given Input
+   * @param {Agent} input Input on which filtering needs to happen as implementing in individual filters<
+   * @inheritdoc
+   */
+  process(input: Agent): Agent {
+    this.filters.forEach(filter => (input = filter.Execute(input)));
+    return input;
+  }
+}
