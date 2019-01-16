4dc52fe4a0afc2a41268c065eb7b434c7cbb7e3d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-01-16 22:46:14 +0100

    Filter interface

---
+++ src/filter.ts
@@ -0,0 +1,16 @@
+/**
+ * A filter to be registered in the message processing pipeline
+ * @export
+ * @interface Filter
+ * @template T
+ */
+export interface Filter<T> {
+  /**
+   * Filter implementing this method would perform processing on the input type
+   * T
+   * @param {T} input
+   * @returns {T}
+   * @memberof Filter
+   */
+  Execute(input: T): T;
+}
